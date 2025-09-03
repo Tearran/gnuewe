fee97150f70af56724d0c2d52384e9a504d2c391
tearran <tearran@gmail.com>
2025-09-03 22:40:59 +0000

    new file:   public_html/include/about.html

---
+++ public_html/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>GNU ewe</title>
+        <meta name="description" content="A hobby FOSS SPA for bash, html, and a simple python3 web server">
+        <meta name="author" content="Tearran (FOSS project)">
+        <meta name="robots" content="index, follow">
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico">
+        <meta name="theme-color" content="#0366d6">
+        <link rel="stylesheet" href="/css/footer.css">
+        <link rel="stylesheet" href="/css/palette.css">
+        <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body>
+        <?php include 'include/header.html'; ?>
+
+        <?php include 'include/nav.html'; ?>
+
+       <main class="site-main">
+                <?php include 'include/home.html'; ?>
+                <?php include 'include/about.html'; ?>
+                <?php include 'include/configng.html'; ?>
+        </main>
+
+        <?php include 'include/footer.html'; ?>
+
+        <script src="/js/page.js"></script>
+        <script src="/js/loader.js"></script>
+
+</body>
+
+</html>
